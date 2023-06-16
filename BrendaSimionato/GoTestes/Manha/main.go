632d155c15d429d10077acceddb264139aeb98ec
b.simionato <brenda.simionato@mercadolivre.com>
2023-06-16 10:13:08 -0300

    feat: GoTestes - Aula 1 e 2

---
+++ BrendaSimionato/GoTestes/Manha/main.go
@@ -0,0 +1,23 @@
+package main
+
+/*
+Exercicio 1
+Quais são as diferenças entre a caixa branca e a caixa preta?
+
+Alguns tipos de teste caixa preta, como testes de segurança e penetração, podem ser automatizados, mas estes costumam ser casos mais restritos e
+específicos em comparação com os testes do tipo caixa branca que, pela sua natureza, podem ser mais facilmente automatizados.
+
+
+Exercicio 2
+O que é um teste funcional?
+
+Teste funcional é um processo de garantia de qualidade e um tipo de teste de caixa preta que baseia seus casos de teste nas especificações do componente
+de software sob teste. As funções são testadas alimentando-as com entrada e examinando a saída, e a estrutura interna do programa raramente é considerada.
+
+
+Exercicio 3
+Teste de integração é a fase do teste de software em que módulos são combinados e testados em grupo. Ela sucede o teste de unidade, em que os
+módulos são testados individualmente, e antecede o teste de sistema, em que o sistema completo é testado num ambiente que simula o ambiente de produção.
+
+
+*/
